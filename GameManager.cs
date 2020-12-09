e9382ff938327469b750361e852b8ceb4177594a
James <75516539+AFLUFFYP3NGUIN@users.noreply.github.com>
2020-12-09 01:04:24 -0500

    Add files via upload

---
+++ GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+
+    public TextMeshProUGUI gameOverText;
+    public Button restartButton;
+    public bool isGameActive;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        isGameActive = true;
+    }
+
+    public void GameOver()
+    {
+        restartButton.gameObject.SetActive(true);
+        gameOverText.gameObject.SetActive(true);
+        isGameActive = false;
+    }
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
